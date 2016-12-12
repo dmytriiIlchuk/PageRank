4d7708821cd34bec62b8b68659755d9009125ecf
dmytriiilchuk <dima6615@gmail.com>
2016-12-12 23:49:39 +0200

    Adds remaining implementations

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String... args) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File("input.txt"));
+        int size = scanner.nextInt();
+        int n = scanner.nextInt();
+        int[][] edges = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            edges[i][0] = scanner.nextInt();
+            edges[i][1] = scanner.nextInt();
+        }
+        PageRank pagerank = new PageRank(edges, size, 0.85);
+        pagerank.buildGMatrix();
+        double[][] eigenvector = pagerank.eigenvectorOfGMatrix();
+        System.out.print("Eigenvector of G matrix: [");
+        for (int i = 0; i < size; i++) {
+            System.out.print(eigenvector[0][i] + " ");
+        }
+        System.out.println("]");
+        int maxI = 0;
+        for (int i = 0; i < size; i++) {
+            if (eigenvector[0][i] > eigenvector[0][maxI]) {
+                maxI = i;
+            }
+        }
+        System.out.println("The page with number " + (maxI + 1) + " has the highest chance of being visited");
+    }
+}
--- src/PageRank.java
+++ src/PageRank.java
@@ -3,6 +3,7 @@ public class PageRank {
     private double[][] gMatrix;
     private int[] rowElements;
     private double dampingFactor;
+    private double eps = 0.00001;
 
     public PageRank(int size) {
         init(size, 0.85);
@@ -11,8 +12,8 @@ public class PageRank {
     public PageRank(int[][] edges, int size, double dampingFactor) {
         init(size, dampingFactor);
         for (int i = 0; i < edges.length; i++) {
-            references[edges[i][0]][edges[i][1]] = 1;
-            rowElements[edges[i][0]]++;
+            references[edges[i][0] - 1][edges[i][1] - 1] = 1;
+            rowElements[edges[i][0] - 1]++;
         }
     }
 
@@ -31,41 +32,77 @@ public class PageRank {
     private void calcWeight() {
         for (int i = 0; i < references.length; i++) {
             for (int j = 0; j < references.length; j++) {
-                references[i][j] = 1 / rowElements[i];
+                if (rowElements[i] != 0) {
+                    references[i][j] /= rowElements[i];
+                }
             }
         }
     }
 
-    private void buildGMatrix() {
+    public void buildGMatrix() {
+        calcWeight();
         int n = references.length;
-        double[][] e = new double[1][n];
-        double[][] et = new double[n][1];
+        double[][] e = new double[n][1];
+        double[][] et = new double[1][n];
         for(int i = 0; i < n; i++) {
-            e[0][i] = 1;
             e[i][0] = 1;
+            et[0][i] = 1;
         }
-        double[][] a = new double[1][n];
+        double[][] a = new double[n][1];
         for (int i = 0; i < n; i++) {
-            a[0][i] = 1 - rowElements[i];
+            a[i][0] = (rowElements[i] > 0)? 0 : 1;
         }
         double[][] s = add(references, multiply(a, multiplyByScalar(et, 1.0 / n)));
         gMatrix = add(multiplyByScalar(s, dampingFactor), multiplyByScalar(multiply(e, et), (1.0 - dampingFactor) / n));
-
     }
 
+    public double[][] eigenvectorOfGMatrix() {
+        double[][] y = new double[1][gMatrix.length];
+        int a = 1;
+        for (int i = 0; i < gMatrix.length; i++) {
+            y[0][i] = 1;
+        }
+        double lambda = 1;
+        double prevLambda = -1;
+        double[][] yNext = new double[1][gMatrix.length];
+        while (Math.abs(lambda-prevLambda) > eps) {
+            yNext = multiply(y, gMatrix);
+            prevLambda = lambda;
+            lambda = yNext[0][0]/y[0][0];
+            y = yNext;
+        }
+        return yNext;
+    }
     private double[][] multiply(double[][] a, double[][] b) {
-        double[][] result = new double[a.length][a.length];
+        double[][] result = new double[a.length][b[0].length];
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < b[0].length; j++) {
+                for (int k = 0; k < b.length; k++) {
+                    result[i][j] += a[i][k] * b[k][j];
+                }
+            }
+        }
         return result;
     }
 
 
     private double[][] add(double[][] a, double[][] b) {
-        double[][] result = new double[a.length][a.length];
+        double[][] result = new double[a.length][a[0].length];
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[0].length; j++) {
+                result[i][j] = a[i][j] + b[i][j];
+            }
+        }
         return result;
     }
 
     private double[][] multiplyByScalar(double[][] a, double b) {
-        double[][] result = new double[a.length][a.length];
+        double[][] result = new double[a.length][a[0].length];
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[0].length; j++) {
+                result[i][j] = a[i][j] * b;
+            }
+        }
         return result;
     }
 
